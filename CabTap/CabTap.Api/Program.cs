057f9c4c1aa51666ff0f150ac03a9ff695c53f2b
rnjesus <veskotodorov786@proton.me>
2024-01-15 16:33:19 +0200

    configure identity settings

--- CabTap/CabTap.Api/Program.cs
+++ CabTap/CabTap.Api/Program.cs
@@ -11,7 +11,25 @@ builder.Services.AddDbContext<IdentityApiDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+    {
+        options.Password.RequiredLength = 8;
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequiredUniqueChars = 1;
+        options.Password.RequireNonAlphanumeric = true;
+
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+        options.Lockout.AllowedForNewUsers = true;
+                
+        options.SignIn.RequireConfirmedAccount = false;
+        options.SignIn.RequireConfirmedEmail = false;
+        options.SignIn.RequireConfirmedPhoneNumber = false;
+
+        options.User.RequireUniqueEmail = true;
+    })
     .AddEntityFrameworkStores<IdentityApiDbContext>()
     .AddDefaultTokenProviders();
 
