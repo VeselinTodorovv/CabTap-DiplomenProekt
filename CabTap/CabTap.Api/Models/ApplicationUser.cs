269fdca673cea7461032cb766359ee459c724137
Veselin Todorov <veskotodorov786@proton.me>
2024-01-15 10:15:28 +0200

    added DbContext and custom user

---
+++ CabTap/CabTap.Api/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace CabTap.Api.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string FirstName { get; init; } = null!;
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string LastName { get; init; } = null!;
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Address { get; init; } = null!;
+    }
+}
---
+++ CabTap/CabTap.Api/Models/IdentityApiDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CabTap.Api.Models
+{
+    public class IdentityApiDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityApiDbContext(DbContextOptions<IdentityApiDbContext> options)
+        : base(options)
+        { }
+    }
+}
