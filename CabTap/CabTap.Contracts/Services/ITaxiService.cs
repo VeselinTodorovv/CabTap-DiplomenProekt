d3046c4ff4a8df45666d8bd0d5630e434606ec45
rnjesus <veskotodorov786@proton.me>
2024-01-17 16:41:51 +0200

    define taxi service

--- CabTap/CabTap.Contracts/Services/ITaxiService.cs
+++ CabTap/CabTap.Contracts/Services/ITaxiService.cs
@@ -1,6 +1,12 @@
+using CabTap.Shared.Taxi;
+
 namespace CabTap.Contracts.Services;
 
 public interface ITaxiService
 {
-    
+    Task<IEnumerable<TaxiAllViewModel>> GetAllAvailableTaxisAsync();
+    Task<TaxiDetailsViewModel> GetTaxiByIdAsync(int taxiId);
+    Task AddTaxiAsync(TaxiCreateViewModel taxiViewModel);
+    Task UpdateTaxiAsync(TaxiEditViewModel taxiViewModel);
+    Task DeleteTaxiAsync(int taxiId);
 }
--- CabTap/CabTap.Services/Services/TaxiService.cs
+++ CabTap/CabTap.Services/Services/TaxiService.cs
@@ -1,5 +1,6 @@
 using CabTap.Contracts.Repositories;
 using CabTap.Contracts.Services;
+using CabTap.Shared.Taxi;
 
 namespace CabTap.Services.Services;
 
@@ -10,5 +11,30 @@ public class TaxiService : ITaxiService
     public TaxiService(ITaxiRepository taxiRepository)
     {
         _taxiRepository = taxiRepository;
+    }
+
+    public async Task<IEnumerable<TaxiAllViewModel>> GetAllAvailableTaxisAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<TaxiDetailsViewModel> GetTaxiByIdAsync(int taxiId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task AddTaxiAsync(TaxiCreateViewModel taxiViewModel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateTaxiAsync(TaxiEditViewModel taxiViewModel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteTaxiAsync(int taxiId)
+    {
+        throw new NotImplementedException();
     }
 }
