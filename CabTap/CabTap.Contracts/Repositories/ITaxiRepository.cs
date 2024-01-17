62694050891c0a882b85583cfdc0ca58876f0c43
rnjesus <veskotodorov786@proton.me>
2024-01-17 15:35:36 +0200

    Implement Taxi Repository

--- CabTap/CabTap.Contracts/Repositories/ITaxiRepository.cs
+++ CabTap/CabTap.Contracts/Repositories/ITaxiRepository.cs
@@ -1,6 +1,12 @@
+using CabTap.Core.Entities;
+
 namespace CabTap.Contracts.Repositories;
 
 public interface ITaxiRepository
 {
-    
+    Task<IEnumerable<Taxi?>> GetAllTaxisAsync();
+    Task<Taxi> GetTaxiByIdAsync(int taxiId);
+    Task AddTaxiAsync(Taxi taxi);
+    Task UpdateTaxiAsync(Taxi taxi);
+    Task DeleteTaxiAsync(int taxiId);
 }
--- CabTap/CabTap.Data/Repositories/TaxiRepository.cs
+++ CabTap/CabTap.Data/Repositories/TaxiRepository.cs
@@ -1,4 +1,6 @@
 using CabTap.Contracts.Repositories;
+using CabTap.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CabTap.Data.Repositories;
 
@@ -9,5 +11,38 @@ public class TaxiRepository : ITaxiRepository
     public TaxiRepository(ApplicationDbContext context)
     {
         _context = context;
+    }
+
+    public async Task<IEnumerable<Taxi?>> GetAllTaxisAsync()
+    {
+        return await _context.Taxis.ToListAsync();
+    }
+
+    public async Task<Taxi> GetTaxiByIdAsync(int taxiId)
+    {
+        return await _context.Taxis.FindAsync(taxiId);
+    }
+
+    public async Task AddTaxiAsync(Taxi taxi)
+    {
+        _context.Taxis.Add(taxi);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateTaxiAsync(Taxi taxi)
+    {
+        _context.Entry(taxi).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+    }
+
+    public async Task DeleteTaxiAsync(int taxiId)
+    {
+        var taxiToRemove = await _context.Taxis.FindAsync(taxiId);
+        if (taxiToRemove != null)
+        {
+            _context.Taxis.Remove(taxiToRemove);
+            await _context.SaveChangesAsync();
+        }
     }
 }
