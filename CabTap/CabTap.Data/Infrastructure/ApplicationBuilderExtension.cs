918db8aa08c1e58aeecd8b9c10d0ad95c02d16ba
Veselin Todorov <veskotodorov786@proton.me>
2024-01-29 09:29:48 +0200

    Make categories seeding async

--- CabTap/CabTap.Data/Infrastructure/ApplicationBuilderExtension.cs
+++ CabTap/CabTap.Data/Infrastructure/ApplicationBuilderExtension.cs
@@ -1,23 +1,21 @@
 using CabTap.Core.Entities;
+using CabTap.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-
-namespace CabTap.Data.Infrastructure;
 
 public static class ApplicationBuilderExtension
 {
     public static async Task<IApplicationBuilder> PrepareDatabase(this IApplicationBuilder app)
     {
         using var serviceScope = app.ApplicationServices.CreateScope();
-        
         var services = serviceScope.ServiceProvider;
-        
-        SeedCategories(services.GetRequiredService<ApplicationDbContext>());
-        
+
+        await SeedCategoriesAsync(services.GetRequiredService<ApplicationDbContext>());
+
         return app;
     }
 
-    private static async void SeedCategories(ApplicationDbContext context)
+    private static async Task SeedCategoriesAsync(ApplicationDbContext context)
     {
         if (context.Categories.Any())
         {
@@ -25,14 +23,14 @@ public static class ApplicationBuilderExtension
         }
 
         context.Categories.AddRange(
-            new Category {Name = "Standard"},
-            new Category {Name = "Premium"},
-            new Category {Name = "Luxury"},
-            new Category {Name = "Economy"},
-            new Category {Name = "Eco-friendly"},
-            new Category {Name = "Business"}
+            new Category { Name = "Standard" },
+            new Category { Name = "Premium" },
+            new Category { Name = "Luxury" },
+            new Category { Name = "Economy" },
+            new Category { Name = "Eco-friendly" },
+            new Category { Name = "Business" }
         );
-        
+
         await context.SaveChangesAsync();
     }
 }
