beecadcc65bbbc876455e5767b2527a7737ff670
Veselin Todorov <veskotodorov786@proton.me>
2024-02-02 09:17:50 +0200

    Driver views/models improvements

--- CabTap/CabTap.Shared/Driver/DriverEditViewModel.cs
+++ CabTap/CabTap.Shared/Driver/DriverEditViewModel.cs
@@ -1,16 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CabTap.Shared.Driver;
 
 public class DriverEditViewModel
 {
     public string Id { get; set; } = null!;
 
+    [Required]
     public string Name { get; set; } = null!;
     
     public DateTime CreatedOn { get; set; }
-    
     public DateTime LastModifiedOn { get; set; }
-    
-    public string CreatedBy { get; set; } = null!;
-    
-    public string LastModifiedBy { get; set; } = null!;
+    public string CreatedBy { get; set; }
+    public string LastModifiedBy { get; set; }
 }
